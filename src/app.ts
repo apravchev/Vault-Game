d18a1ea34d95b06a52135f2002b283f3bc188de0
alexeod <90037308+apravchev@users.noreply.github.com>
2024-06-11 00:46:03 +0300

    feat: added vite config and app entry point

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import * as PIXI from "pixi.js";
+import config from "./config";
+
+document.addEventListener("DOMContentLoaded", () => {
+  //Start Game from this point
+});
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+};
+
+export default config;
